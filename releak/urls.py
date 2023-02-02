fc8fb7310b57c669a8d0ca5b272dd18069d5b44a
Antoine Baroux <antoine.baroux@gmx.fr>
2023-02-02 17:18:39 +0100

    page d'accueil

--- releak/urls.py
+++ releak/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from releak import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('releak/', views.home)
 ]
+
